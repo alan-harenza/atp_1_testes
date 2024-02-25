596fc79343df87d6767e1cdd1b74bc9cb708de39
alan-harenza <111076434+alan-harenza@users.noreply.github.com>
2024-02-25 11:25:14 -0300

    Aprimoramento exercícios 1 e 2
    Aprimoramento dos exercícios 1 e 2, garantindo que a variáveis tenham o tipo correto.

--- calculo_novo_valor_produto.py
+++ calculo_novo_valor_produto.py
@@ -8,6 +8,10 @@ tipo variável:  valor produto - int ou float
 
 
 def novo_valor_produto(valor_produto):
-    valor_desconto = valor_produto * (9/100)
-    novo_valor_produto = valor_produto - valor_desconto
-    return novo_valor_produto, valor_desconto
+    if isinstance(valor_produto, (int, float)):
+        valor_desconto = valor_produto * (9/100)
+        novo_valor_produto = valor_produto - valor_desconto
+        return novo_valor_produto, valor_desconto
+
+    else:
+        raise TypeError
--- calculo_salario_liquido.py
+++ calculo_salario_liquido.py
@@ -9,6 +9,9 @@ Tipo de cada variável - valor_hora_aula - float
 
 
 def calculo_salario_liquido(valor_hora_aula, nro_aulas, percentual_inss):
-    salario_bruto = valor_hora_aula * nro_aulas
-    salario_liquido = salario_bruto * (1 - (percentual_inss/100))
-    return salario_liquido
+    if isinstance(valor_hora_aula, (int, float)) and isinstance(nro_aulas, int) and isinstance(percentual_inss, (int, float)):
+        salario_bruto = valor_hora_aula * nro_aulas
+        salario_liquido = salario_bruto * (1 - (percentual_inss/100))
+        return salario_liquido
+    else:
+        raise TypeError
